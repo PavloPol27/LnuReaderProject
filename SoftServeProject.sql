eb9cc34debf7151e1d5ffd20daa8a33d6902e06b
IhorMruchko <ihormruchko666@gmail.com>
2021-03-15 19:34:44 +0200

    Add file SQL with simple database

---
+++ SoftServeProject.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE BookProject;
+
+CREATE DOMAIN not_negative_page as int
+	CHECK (value > 0);
+
+DROP TABLE IF EXISTS Book CASCADE;
+CREATE TABLE Book(
+	file_path text PRIMARY KEY,
+	file_name text,
+	file_stoped_page not_negative_page
+);
+
+DROP TABLE IF EXISTS Note CASCADE;
+CREATE TABLE Note(
+	note_id serial PRIMARY KEY,
+	note_text text,
+	note_page not_negative_page,
+	note_line int,
+	note_in_book text REFERENCES Book
+);
+
+DROP TABLE IF EXISTS Quotation CASCADE;
+CREATE TABLE Quotation(
+	quote_id serial PRIMARY KEY,
+	quote_text text,
+	quote_page not_negative_page,
+	quote_line int,
+	quote_in_book text REFERENCES Book
+);
+
+DROP TABLE IF EXISTS Bookmark CASCADE;
+CREATE TABLE Bookmark(
+	bookmark_id serial PRIMARY KEY,
+	bookmark_page not_negative_page,
+	bookmark_in_book text REFERENCES Book
+);
+
+
+
+
+
+
+
