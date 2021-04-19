bdb765a7ac0a50187d317e39148e1940443fcd95
IhorMruchko <ihormruchko666@gmail.com>
2021-04-19 22:16:29 +0300

    Added file with interface extension

---
+++ design/main_menu_extension.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QApplication
+from main_menu import MainWindow
+import sys
+import logging
+
+class WidnowInteractivity(MainWindow):
+    def __init__():
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = WidnowInteractivity()
+    ex.show()
+    sys.exit(app.exec_())
