c2b22b70016a2b980404d2ec8c00cafb0acfdf10
IhorMruchko <ihormruchko666@gmail.com>
2021-04-20 11:55:06 +0300

    Added drag and drop functions

--- design/main_menu_extension.py
+++ design/main_menu_extension.py
@@ -9,28 +9,54 @@ logging.basicConfig(filename = "ReaderLogger.log", level = logging.INFO,
 class WidnowInteractivity(MainWindow):
     def __init__(self):
         super().__init__()
+        self.accaptableFormats = ['.pdf', '.epub', '.fb2']
         self.filesDirectories = []
 
 #------------------------------------------------------
 #------------------Open File--------------------------
 #------------------------------------------------------
         QShortcut("Ctrl+O", self).activated.connect(self.openFile)
-
         self.addBookQButton.clicked.connect(self.openFile)
+
+        #drag and drop
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, event):
+        url = str(event.mimeData().urls()[0])
+        if any([format in url for format in self.accaptableFormats]):
+            event.accept()
+            logging.info(f"file's url = {url} was added")
+        else:
+            event.ignore()
+            logging.info(f"file's url = {url} was declined")
+
+    def dropEvent(self, event):
+        urls = [str(url) for url in event.mimeData().urls()]
+        urls = [url[url.find("'")+1:url.rfind("'")].replace(r"file:///", '') for url in urls]
+        for url in urls:
+            if url in self.filesDirectories:
+                event.ignore()
+                logging.info(f"file's url = {url} was declined")
+            else:
+                self.filesDirectories.append(url)
+                logging.info(f"file's url = {url} was added")
+
 
     def openFile(self):
         directories, _ = QFileDialog.getOpenFileNames(self, filter = 'PDF (*.pdf);; FB2 (*.fb2);; EPUB (*.epub)')
+        self.addFile(directories)
+            # TODO:
+            # add parsing book's metadata;
+            # place information into table;
+            # insert information into database;
+
+    def addFile(self, directories):
         for directory in directories:
             if directory not in self.filesDirectories:
                 self.filesDirectories.append(directory)
                 logging.info(f"Directory {directory} was added to list.")
             else:
                 logging.info(f"Directory {directory} was ignored.")
-            # TODO:
-            # add parsing book's metadata;
-            # place information into table;
-            # insert information into database;
-
 
 
 
