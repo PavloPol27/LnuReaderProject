57f4dca1ed0877a6d71c364d80dc3f48e2878419
IhorMruchko <ihormruchko666@gmail.com>
2021-04-20 21:35:17 +0300

    Have done dialog class

--- design/dialog_confirm_decline.py
+++ design/dialog_confirm_decline.py
@@ -1,14 +1,29 @@
 from PyQt5.QtWidgets import QDialog, QApplication, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
 import sys
+
 
 
 class ConfirmDialog(QDialog):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Deleting File')
-        self.setWindowIcon(QIcon('images/removeBook.png'))
-        self.setFixedSize(400, 200)
+        
+        #craete the placeholder for
+        self.dialogBackgroundVLayout = QVBoxLayout() #all widgets and layours
+        self.buttonHLayout = QHBoxLayout() #buttons
+        
+        self.changeStyle()
+
+        self.layout_init()
+        self.buttons_init()
+
+        self.setLayout(self.dialogBackgroundVLayout)
+    
+    def changeStyle(self):
+        self.setWindowTitle('Deleting file')
+        self.setWindowIcon(QIcon('images/removeBook.ico'))
+        self.setFixedSize(300, 100)
         self.setStyleSheet('''
         QDialog {
                 color: #5c5c5c;
@@ -34,29 +49,31 @@ class ConfirmDialog(QDialog):
                 }
                 QDialog QLabel
                 {
-                font-size: 25px;
+                font-size: 17px;
                 }
                 
         """
         '''
             )
-        self.dialogBackgroundVLayout = QVBoxLayout()
-        self.buttonHLayout = QHBoxLayout()
-        self.warningLable = QLabel("Are you really want to delete file?")
-        
+
+    def buttons_init(self):
+        #create buttons
         self.acceptButton = QPushButton("Yes!")
         self.declineButton = QPushButton("No")
 
-
-        self.dialogBackgroundVLayout.addWidget(self.warningLable)
-        self.buttonHLayout.addWidget(self.acceptButton)
-        self.buttonHLayout.addWidget(self.declineButton)
-
+        #bind them to closing files function
         self.declineButton.clicked.connect(lambda: self.done(0))
         self.acceptButton.clicked.connect(lambda: self.done(1))
 
+        #add buttons to layout
+        self.buttonHLayout.addWidget(self.acceptButton)
+        self.buttonHLayout.addWidget(self.declineButton)
+
+    def layout_init(self):
+        self.warningLable = QLabel("Are you really want to delete file?")
+        self.warningLable.setAlignment(Qt.AlignHCenter|Qt.AlignCenter)
+        self.dialogBackgroundVLayout.addWidget(self.warningLable)
         self.dialogBackgroundVLayout.addLayout(self.buttonHLayout)
-        self.setLayout(self.dialogBackgroundVLayout)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
