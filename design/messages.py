0d62635bd42c9a27a93ae61035789695fc842c42
IhorMruchko <ihormruchko666@gmail.com>
2021-04-22 12:05:12 +0300

    Created WarningMessage class.

--- design/messages.py
+++ design/messages.py
@@ -9,3 +9,14 @@ class ErrorMessage(QMessageBox):
         self.setInformativeText(info)
         self.setWindowTitle("Error")
         self.exec_()
+
+
+class WarningMessage(QMessageBox):
+    def __init__(self, text='', info=''):
+        super().__init__()
+
+        self.setIcon(QMessageBox.Warning)
+        self.setText(text)
+        self.setInformativeText(info)
+        self.setWindowTitle("Warning")
+        self.exec_()
